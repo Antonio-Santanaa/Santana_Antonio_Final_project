6c826ab900173a8ed38399e4794ec1b33260c3af
Antonio-Santanaa <antonio.santana25@bcp.org>
2023-12-05 12:20:45 -0800

    Pushed ReadME

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 # https://stackoverflow.com/questions/26767591/pygame-error-video-system-not-initialized
 # https://www.simplilearn.com/tutorials/python-tutorial/python-for-loop#:~:text=The%20for%20loop%20in%20Python,tuple%2C%20array%2C%20or%20string.&text=The%20program%20operates%20as%20follows,item%20in%20our%20iterable%20object. 
 # https://pygame.readthedocs.io/en/latest/1_intro/intro.html
+# https://stackoverflow.com/questions/26767591/pygame-error-video-system-not-initialized
 
 # Import Libraries & Moduels 
 import pygame as pg 
@@ -38,31 +39,25 @@ class Game:
 
 
 # Strings
-class string():
-    def __init__(self,x,y,color1,color2,key):
-        self.x = x
-        self.y = y 
-        self. color1 = color1
-        self. color2 = color2
-        self.key = key 
-        self.rect = pg.Rect(self.x,self.y,50,20)
-strings = [
+    class string():
+        def __init__(self,x,y,color1,color2,key):
+            self.x = x
+            self.y = y 
+            self. color1 = color1
+            self. color2 = color2
+            self.key = key 
+            self.rect = pg.Rect(self.x,self.y,50,20)
+    strings = [
     string(100,1200,(255,0,0),(220,0,0),pg.K_s),
     string(200,1200,(0,255,0),(0,220,0),pg.K_d),
     string(300,1200,(0,0,255),(0,0,220),pg.K_f),
     string(400,1200,(255,255,0),(220,220,0),pg.K_j),
     string(500,1200,(0,255,255),(0,220,220),pg.K_k),
     string(600,1200,(255,0,255),(220,0,220),pg.K_l),]
-def draw(self):
-    self.screen.fill(BLACK)
-    for string in strings:
-        pg.draw.rect(self.screen,string.color1,string.rect)
-
-
-
-
-
-
+    def draw(self):
+        self.screen.fill(BLACK)
+        for string in strings:
+            pg.draw.rect(self.screen,string.color1,string.rect)
 
 while True:
     for event in pg.event.get():
@@ -72,4 +67,4 @@ while True:
 
 pg.display.update()
 
-# etetete
+# etetete   
